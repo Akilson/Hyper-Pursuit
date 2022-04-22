133533e4c45823d7d9848439959a07d8e7abc5f0
Quentin Rataud <quentin.rataud@gmail.com>
2022-04-22 23:41:30 +0200

    Add player detection with tag Remove materials in portals prefabs Add portal prefabs for both black and white player Update Game scene with new portals

--- Assets/Scripts/Portal/PortalCamera.cs
+++ Assets/Scripts/Portal/PortalCamera.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class PortalCamera : MonoBehaviour
 {
-    public GameObject player;
+    public string playerTag;
+    private GameObject player;
     public Transform portal1;
     public Transform portal2;
     
     private Transform camera;
 
     void Start(){
-        camera = player.transform.GetChild(0).GetChild(0);
+        player = null;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player is null)
+        {
+            player = GameObject.FindGameObjectWithTag(playerTag);
+            if (player is not null)
+                camera = player.transform.GetChild(0).GetChild(0);
+        }
+        else
+        {
+            Debug.Log(player);
+            transform.position = camera.position;
+            transform.rotation = camera.rotation;
 
-    transform.position = camera.position;
-    transform.rotation = camera.rotation;
+        //float angle = -Quaternion.Angle(portal1.rotation, portal2.rotation);
+            float angle = portal2.transform.eulerAngles.y - portal1.transform.eulerAngles.y;
 
-    //float angle = -Quaternion.Angle(portal1.rotation, portal2.rotation);
-    float angle = portal2.transform.eulerAngles.y - portal1.transform.eulerAngles.y;
+            transform.RotateAround(portal1.position, Vector3.up, angle);
+            Vector3 offset = portal2.position - portal1.position;
 
-    transform.RotateAround(portal1.position, Vector3.up, angle);
-    Vector3 offset = portal2.position - portal1.position;
-
-    transform.position += offset; 
-
-    }   
+            transform.position += offset; 
+        }   
+    }
 }
