ebf54e1d1cd3689d33a6445db2c20aa9fed3d07c
Quentin Rataud <quentin.rataud@gmail.com>
2022-04-23 22:36:52 +0200

    Creation of the room

--- Assets/Scripts/Portal/PortalCamera.cs
+++ Assets/Scripts/Portal/PortalCamera.cs
@@ -16,6 +16,7 @@ public class PortalCamera : MonoBehaviour
         player = null;
         playerCam = null;
         white = PhotonNetwork.IsMasterClient;
+        white = true;
     }
 
     // Update is called once per frame
